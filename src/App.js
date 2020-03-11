2649b6c9315ae224703c0438f92d3960d6cce57a
Ilya <ilyaphotoshop17@gmail.com>
2020-03-11 16:06:16 +0200

    starting template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+      <div>
+          <Header/>
+        <h2>TCP</h2>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+    return(
+        <div>
+            <h2>Header</h2>
+            <p>.</p>
+        </div>
+    )
+};
+
+export default Header
