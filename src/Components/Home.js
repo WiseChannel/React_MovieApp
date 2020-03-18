c18d84eb262e492b68bf07c3b4e8ef1cc10c7fe2
Ilya <ilyaphotoshop17@gmail.com>
2020-03-18 13:08:22 +0200

    [add] added image directory and worked in MovierThumb/Home/Grid components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+
+//import Config
 import {
         API_URL,
         API_KEY,
@@ -18,28 +20,50 @@ import Spinner from "./Elements/Spinner";
 //import custom hook
 import { useHomeFetch } from "./Hooks/useHomeFetch";
 
+import NoImage from './images/no_image.jpg'
+
 const Home = () => {
-    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
-    console.log(state)
+    const [
+        {
+            state: {movies, currentPage, totalPages, heroImage}, 
+            loading, 
+            error 
+        }, 
+        fetchMovies] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('')
+
 
     if(error) return <div>New error in Home</div>
 
-    if(!state.movies[0]) return <Spinner/>
+    if(!movies[0]) return <Spinner/>
 
-    return(
+    return (
         <>
-         <HeroImage
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-            title={state.heroImage.original_title}
-            text={state.heroImage.overview}
-         />
-         <SearchBar/>
-         <Grid/>
-         <MovieThumb/>
-         <Spinner/>
-         <LoadMore/>
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+            title={heroImage.original_title}
+            text={heroImage.overview}
+          />
+          <SearchBar />
+          <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+            {movies.map(movie => (
+              <MovieThumb
+                key={movie.id}
+                clickable
+                image={
+                  movie.poster_path
+                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                    : NoImage
+                }
+                movieId={movie.id}
+                movieName={movie.original_title}
+              />
+            ))}
+          </Grid>
+          {loading && <Spinner />}
+          <LoadMore text="Load More" callback={LoadMore} />
         </>
-    )
+      );
 };
 
 export default Home
