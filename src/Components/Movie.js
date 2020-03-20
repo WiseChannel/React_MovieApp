d64e669a7a05a29b39e0a9083ebf02500e77a936
Ilya <ilyaphotoshop17@gmail.com>
2020-03-20 22:36:43 +0200

    [add] scaffolding new movie page

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
-const Movie = ({ movieId }) => <div>Movie: {movieId}</div>;
+//Components
+import Navigation from "./Elements/Navigation";
+import MovieInfo from "./Elements/MovieInfo";
+import MovieInfoBar from "./Elements/MovieInfoBar";
+import Actor from "./Elements/Actor";
+import Grid from "./Elements/Grid";
+import Spinner from "./Elements/Spinner";
+
+const Movie = ({ movieId }) => (
+    <>
+        <Navigation/>
+        <MovieInfo/>
+        <MovieInfoBar/>
+        <Grid>
+            <Actor/>
+        </Grid>
+        <Spinner/>
+    </>
+);
 
 export default Movie
 
