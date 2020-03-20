05db5b60c5660731c68c5cb54af71aab5b1d2be2
Ilya <ilyaphotoshop17@gmail.com>
2020-03-20 22:23:06 +0200

    [add] added two new components(NotFound/Movier) and made routing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from "./Components/Header/Header";
 
-import {GlobalStyle} from './Components/styles/GlobalStyle';
+import { GlobalStyle } from './Components/styles/GlobalStyle';
+import { Router } from '@reach/router';
 
 import Home from "./Components/Home";
+import Movie from "./Components/Movie";
+import NotFound from "./Components/NotFound";
 
 
 const App = () => {
   return(
       <>
         <Header/>
-        <Home/>
+        <Router>
+            <Home path="/"/>
+            <Movie path="/:movieId" />
+            <NotFound default />
+        </Router>
         <GlobalStyle/>
     </>
   )
-}
+};
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-import {StyledHeader } from '../styles/StyledHeader'
+import { StyledHeader } from '../styles/StyledHeader'
+import { Link } from '@reach/router';
 
-//import NewLogo from '../images/...'       *Нарисовать логотип* 
+//import NewLogo from '../images/...'       *Нарисовать логотип*
 
 const Header = () => {
     return(
         <StyledHeader>
             <div className='header-content'>
-                <h2>React Movie</h2>   
+                <Link to="/">
+                    <h2>React Movie</h2>
+                </Link>
                 <h2>WiseChannel</h2>
             </div>
         </StyledHeader>
